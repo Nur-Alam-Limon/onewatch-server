8b69e5860238fd4a0c6df970decd8ddc3b8ab983
Nur-Alam-Limon <nuralam201425@gmail.com>
2023-11-04 14:55:06 +0600

    Changed port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5002;
 
 const { MongoClient } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xpwab.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
